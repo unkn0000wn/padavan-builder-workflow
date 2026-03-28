91971448f019d79cce17b37c9a76cec9a223bc3b
unkn0000wn <40450301+unkn0000wn@users.noreply.github.com>
2026-03-28 19:50:14 +0300

    Update post-build.sh

--- post-build.sh
+++ post-build.sh
@@ -1,6 +1,10 @@
-FW_FILE_NAME="$(find padavan-ng-hadzhioglu/trunk/images -type f -regextype posix-extended -iregex ".*\.(trx|bin)$" \
-                        -printf "%T@\t%f\n" | sort -V | tail -1 | cut -f2)"
+FW_FILE_NAME="$(
+  find padavan-ng-hadzhioglu/trunk/images \
+    -type f -regextype posix-extended \
+    -iregex '.*\.\(trx\|bin\)$' \
+    -printf '%T@\t%f\n' | sort -V | tail -1 | cut -f2
+)"
 cp "padavan-ng-hadzhioglu/trunk/images/$FW_FILE_NAME" .
-echo "FW_FILE_NAME=$FW_FILE_NAME" >> $GITHUB_ENV
+echo "FW_FILE_NAME=$FW_FILE_NAME" >> "$GITHUB_ENV"
 stat "padavan-ng-hadzhioglu/trunk/images/$FW_FILE_NAME"
 
