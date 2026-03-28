920bfa74ffecf6767504bd76f5602c129082bb6e
unkn0000wn <40450301+unkn0000wn@users.noreply.github.com>
2026-03-28 19:53:10 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,30 +1,8 @@
-# ln -snf /usr/share/zoneinfo/Africa/Nairobi /etc/localtime
-# apt update
-# DEBIAN_FRONTEND=noninteractive apt install tzdata -y
-# date
-
-#!/bin/bash
-set -e  # Остановка при ошибке
-
 echo "=== Pre-build start: $(date) ==="
-
 # 1. Timezone Москва
-apt update -qq
-DEBIAN_FRONTEND=noninteractive apt install tzdata -yqq
 ln -snf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
+apt update
+DEBIAN_FRONTEND=noninteractive apt install tzdata -y
 export TZ=Europe/Moscow
 date  # Проверка: Thu Mar 26 14:27:00 MSK 2026
-
-# 2. Обновление Zapret (pull/clone свежую версию)
-echo "Updating Zapret..."
-mkdir -p user/zapret
-cd user/zapret
-if [ -d .git ]; then
-    git pull origin master --ff-only || git reset --hard origin/master
-else
-    git clone https://github.com/bol-van/zapret.git .
-fi
-echo "Zapret latest: $(git log -1 --oneline | head -1)"
-git status -s  # Показать изменения
-
 
